e8bf83475c34d52179eb36589033712ec67a31dc
sebaek <sebaek@gmail.com>
2020-11-12 16:32:07 +0900

    jdbc

---
+++ src/chap14/EmployeeDao.java
@@ -0,0 +1,72 @@
+package chap14;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+public class EmployeeDao {
+	public static List<String> listEmployeename() {
+		List<String> list = new ArrayList<>();
+		Connection conn = null;
+		Statement stmt = null;
+		ResultSet rs = null;
+		
+		String url = "jdbc:oracle:thin:@localhost:1521:orcl";
+		String user = "c##mydbms";
+		String password = "admin";
+		
+		String sql = "SELECT ename FROM employee";
+		
+		
+		try {
+			// 1. 클래스 로딩
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			
+			// 2. 커넥션 생성
+			conn = DriverManager.getConnection(url, user, password);
+			
+			// 3. Statment 생성
+			stmt = conn.createStatement();
+			
+			// 4. 쿼리 실행
+			rs = stmt.executeQuery(sql);
+			
+			// 5. 결과 처리
+		
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			// 6. statment 닫고
+			try {
+				if (stmt != null) {
+					stmt.close();
+				}
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			// 7. connection 닫고
+			try {
+				if (conn != null) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+		return list;
+	}
+}
+
+
+
+
+
+
