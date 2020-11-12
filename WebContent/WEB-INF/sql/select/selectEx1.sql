99a9e687084fea019adefc56d0b8366228410b41
sebaek <sebaek@gmail.com>
2020-11-12 10:39:31 +0900

    sql

---
+++ WebContent/WEB-INF/sql/select/selectEx1.sql
@@ -0,0 +1,30 @@
+SELECT * FROM employee;
+SELECT * FROM bonus;
+SELECT * FROM department;
+SELECT * FROM salgrade;
+
+SELECT ename FROM employee;
+SELECT eno, ename FROM employee;
+SELECT ename, eno FROM employee;
+SELECT ename, salary FROM employee;
+SELECT ename, salary, salary*12 FROM employee;
+SELECT ename, salary / 100 FROM employee;
+SELECT ename, salary + 100 FROM employee;
+SELECT ename, salary - 100 FROM employee;
+SELECT ename, salary, salary*12 AS 연봉 FROM employee;
+SELECT ename, salary, salary*12 연봉 FROM employee;
+-- 한 줄 주석
+/*
+  여러줄 주석
+*/
+SELECT * FROM employee;
+SELECT ename, salary, commission, salary + commission FROM employee;
+SELECT ename, salary, commission, NVL(commission, 0) FROM employee;
+SELECT ename, salary + NVL(commission, 0) FROM employee;
+SELECT ename, salary + NVL(commission, 0) 합 FROM employee;
+
+
+/*
+SELECT [DISTINCT] {*|column[alia]...}
+
+*/
