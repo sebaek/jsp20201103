27d6551835b2aa48643ecb258004dc964c0a9a75
sebaek <sebaek@gmail.com>
2020-11-13 10:35:26 +0900

    order by

--- WebContent/WEB-INF/sql/select/selectEx3.sql
+++ WebContent/WEB-INF/sql/select/selectEx3.sql
@@ -10,3 +10,22 @@ SELECT * FROM employee;
 SELECT * FROM employee WHERE commission = NULL;
 SELECT * FROM employee WHERE commission IS NULL;
 SELECT * FROM employee WHERE commission IS NOT NULL;
+
+SELECT * FROM employee
+ORDER BY ename;
+SELECT * FROM employee ORDER BY eno;
+SELECT * FROM employee 
+WHERE salary > 2000 
+ORDER BY eno;
+
+SELECT * FROM employee
+ORDER BY eno ASC;
+SELECT * FROM employee
+ORDER BY eno DESC;
+
+SELECT dno, ename FROM employee
+ORDER BY dno, ename;
+
+SELECT dno, ename FROM employee
+ORDER BY dno DESC, ename ASC;
+
