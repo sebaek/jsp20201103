10d07d886114af3d43712879f41860c324756e63
sebaek <sebaek@gmail.com>
2020-11-13 16:31:08 +0900

    funcionts

--- WebContent/WEB-INF/sql/select/functionEx3.sql
+++ WebContent/WEB-INF/sql/select/functionEx3.sql
@@ -22,7 +22,17 @@ SELECT
 MONTHS_BETWEEN(TO_DATE('2019-10-13', 'yyyy-mm-dd'), SYSDATE)
 FROM dual;
 
+SELECT ADD_MONTHS(sysdate, 12) FROM dual;
+SELECT ADD_MONTHS(sysdate, 6) FROM dual;
+SELECT ADD_MONTHS(sysdate, -12) FROM dual;
 
+SELECT NEXT_DAY(sysdate, '토요일') FROM dual;
+SELECT NEXT_DAY(sysdate, '금요일') FROM dual;
+SELECT NEXT_DAY(sysdate, 6) FROM dual;
 
+select * from employee;
+
+SELECT LAST_DAY(sysdate) FROM dual;
+SELECT LAST_DAY(ADD_MONTHS(sysdate, -9)) FROM dual;
 
 
---
+++ WebContent/WEB-INF/sql/select/functionEx4.sql
@@ -0,0 +1,17 @@
+SELECT TO_CHAR(sysdate) FROM dual;
+SELECT TO_CHAR(sysdate, 'YYYY') FROM dual;
+SELECT TO_CHAR(sysdate, 'YY') FROM dual;
+SELECT TO_CHAR(sysdate, 'MM') FROM dual;
+SELECT TO_CHAR(sysdate, 'DD') FROM dual;
+SELECT TO_CHAR(sysdate, 'MON') FROM dual;
+SELECT TO_CHAR(sysdate, 'YYYY-MM-DD') FROM dual;
+
+SELECT TO_CHAR(sysdate, 'HH') FROM dual;
+SELECT TO_CHAR(sysdate, 'HH24') FROM dual;
+SELECT TO_CHAR(sysdate, 'MI') FROM dual;
+SELECT TO_CHAR(sysdate, 'SS') FROM dual;
+SELECT TO_CHAR(sysdate, 'HH:MI:SS') FROM dual;
+
+SELECT TO_CHAR(sysdate, 'YYYY-MM-DD HH:MI:SS') FROM dual;
+
+
