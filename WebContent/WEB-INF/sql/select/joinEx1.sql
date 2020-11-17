e718b079ef355d750a8790ca2bdaab335d846d3f
sebaek <sebaek@gmail.com>
2020-11-17 09:34:32 +0900

    join

--- WebContent/WEB-INF/sql/select/joinEx1.sql
+++ WebContent/WEB-INF/sql/select/joinEx1.sql
@@ -17,7 +17,21 @@ SELECT eno, ename, employee.dno edno, department.dno ddno, dname
 FROM employee, department 
 WHERE eno = 7369 AND employee.dno = department.dno;
 
+-- equi join
+SELECT * FROM employee, department
+WHERE employee.dno = department.dno;
 
+SELECT * FROM employee, department
+WHERE employee.dno = department.dno
+AND eno = 7369;
+
+SELECT * FROM employee e, department d
+WHERE e.dno = d.dno
+AND e.eno = 7369;
+
+SELECT e.eno, e.ename, e.dno, d.dname 
+FROM employee e, department d
+WHERE e.dno = d.dno;
 
 
 
