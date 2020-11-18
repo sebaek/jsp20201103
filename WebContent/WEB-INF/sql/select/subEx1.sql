6abe1860538733441ec7ab1321b6ae43d833f8e7
sebaek <sebaek@gmail.com>
2020-11-18 12:25:00 +0900

    sub query

--- WebContent/WEB-INF/sql/select/subEx1.sql
+++ WebContent/WEB-INF/sql/select/subEx1.sql
@@ -9,8 +9,41 @@ WHERE eno = (SELECT manager
              FROM employee 
              WHERE eno = 7499);
              
-             
-             
-             
-             
+SELECT ename FROM employee
+WHERE salary > (SELECT salary
+                FROM employee
+                WHERE eno = 7499);
+
+SELECT ename FROM employee
+WHERE salary > (SELECT salary
+                FROM employee);
+                
+-- 다중 행 서브 쿼리
+/*
+IN        값이 하나라도 있으면
+ANY(SOME) 하나 이상이 만족하면
+ALL       모두 만족하면
+*/
+
+SELECT dno 
+FROM department
+WHERE dno <= 20;
+
+SELECT ename, dno 
+FROM employee
+WHERE dno IN (SELECT dno 
+              FROM department
+              WHERE dno <= 20);
+
+
+
+
+
+
+
+
+
+
+
+
              
