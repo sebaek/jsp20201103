c10528395004260fef95be4c1eafd431010f66bf
sebaek <sebaek@gmail.com>
2020-11-26 15:22:30 +0900

    init param

--- src/chap17/ServletEx09.java
+++ src/chap17/ServletEx09.java
@@ -1,31 +1,49 @@
 package chap17;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class ServletEx08
+ * Servlet implementation class ServletEx09
  */
-@WebServlet(value = "/ex08", loadOnStartup = 1)
-public class ServletEx08 extends HttpServlet {
+//@WebServlet("/ex09")
+public class ServletEx09 extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public ServletEx08() {
+    public ServletEx09() {
         super();
         // TODO Auto-generated constructor stub
     }
 
     @Override
     public void init() throws ServletException {
-    	// TODO Auto-generated method stub
-    	System.out.println("init method 실행");
+    	ServletConfig config = getServletConfig();
+    	String url = config.getInitParameter("dburl");
+    	String id = config.getInitParameter("dbid");
+    	String pw = config.getInitParameter("dbpw");
+    	
+    	try {
+				Class.forName("oracle.jdbc.driver.OracleDriver");
+				Connection con = DriverManager.getConnection(url, id, pw);
+				
+				if (con != null) {
+					con.close();
+				}
+				System.out.println("servlet 09 init method 실행");
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
     	
     	super.init();
     }
@@ -35,7 +53,9 @@ public class ServletEx08 extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		System.out.println("ex08 doget method");
+		ServletConfig config = getServletConfig();
+		String url = config.getInitParameter("dburl");
+		System.out.println(url);
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
