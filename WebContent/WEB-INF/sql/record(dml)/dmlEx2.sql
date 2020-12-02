85d50d79215b7e45a63a70b6af256c4283d961b7
sebaek <sebaek@gmail.com>
2020-12-02 14:12:49 +0900

    number

--- WebContent/WEB-INF/sql/record(dml)/dmlEx2.sql
+++ WebContent/WEB-INF/sql/record(dml)/dmlEx2.sql
@@ -1,1 +1,31 @@
- 
+-- 
+CREATE TABLE data_type_ex1
+(
+num1 NUMBER,
+num2 NUMBER(2),
+num3 NUMBER(3, 2)
+);
+INSERT INTO data_type_ex1 (num1)
+VALUES (0);
+INSERT INTO data_type_ex1 (num1)
+VALUES (0.01);
+INSERT INTO data_type_ex1 (num1)
+VALUES (99999999999);
+INSERT INTO data_type_ex1 (num2)
+VALUES (0);
+INSERT INTO data_type_ex1 (num2)
+VALUES (5.5); -- 반올림
+INSERT INTO data_type_ex1 (num2)
+VALUES (99);
+INSERT INTO data_type_ex1 (num2)
+VALUES (100);
+INSERT INTO data_type_ex1 (num3)
+VALUES (0.01);
+INSERT INTO data_type_ex1 (num3)
+VALUES (0.015); -- 반올림
+INSERT INTO data_type_ex1 (num3)
+VALUES (9.015); -- 반올림
+INSERT INTO data_type_ex1 (num3)
+VALUES (10.015); -- x
+COMMIT;
+SELECT * FROM data_type_ex1;
