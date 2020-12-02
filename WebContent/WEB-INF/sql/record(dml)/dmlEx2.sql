d5372b632ec84676a3ca8095a7fca27b99b23523
sebaek <sebaek@gmail.com>
2020-12-02 14:26:39 +0900

    char

--- WebContent/WEB-INF/sql/record(dml)/dmlEx2.sql
+++ WebContent/WEB-INF/sql/record(dml)/dmlEx2.sql
@@ -29,3 +29,31 @@ INSERT INTO data_type_ex1 (num3)
 VALUES (10.015); -- x
 COMMIT;
 SELECT * FROM data_type_ex1;
+
+-- 문자열
+CREATE TABLE data_type_ex2
+(
+char1 CHAR(3),
+vchar2 VARCHAR2(6)
+);
+INSERT INTO data_type_ex2
+(char1) VALUES ('');
+INSERT INTO data_type_ex2
+(char1) VALUES ('a');
+INSERT INTO data_type_ex2
+(char1) VALUES ('abc');
+INSERT INTO data_type_ex2
+(char1) VALUES ('abcd'); -- x
+
+INSERT INTO data_type_ex2
+(vchar2) VALUES ('abc');
+INSERT INTO data_type_ex2
+(vchar2) VALUES ('abcdef');
+INSERT INTO data_type_ex2
+(vchar2) VALUES ('abcdefg');
+
+COMMIT;
+SELECT * FROM data_type_ex2;
+
+
+
