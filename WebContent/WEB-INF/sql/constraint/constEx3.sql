5480107eb16b14a6953f00da8467425c7de0a8e5
sebaek <sebaek@gmail.com>
2020-12-03 16:08:19 +0900

    table

---
+++ WebContent/WEB-INF/sql/constraint/constEx3.sql
@@ -0,0 +1,28 @@
+-- PRIMARY KEY
+CREATE TABLE const09 (
+    id NUMBER,
+    name VARCHAR2(30),
+    PRIMARY KEY (id)
+);
+INSERT INTO const09 (id, name)
+VALUES (null, 'a');
+INSERT INTO const09 (id, name)
+VALUES (1, 'a');
+INSERT INTO const09 (id, name)
+VALUES (1, 'b');
+COMMIT;
+
+-- FOREIGN KEY
+CREATE TABLE const10
+(
+name VARCHAR2(30),
+dno NUMBER(2),
+FOREIGN KEY (dno) REFERENCES const08_dep
+);
+INSERT INTO const10
+VALUES ('a', 10);
+INSERT INTO const10
+VALUES ('b', 50);
+
+
+
