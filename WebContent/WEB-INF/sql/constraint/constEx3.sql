1dd65cb00456b4810c66fcc1c54b7974a0b95e2f
sebaek <sebaek@gmail.com>
2020-12-03 16:23:46 +0900

    constraints

--- WebContent/WEB-INF/sql/constraint/constEx3.sql
+++ WebContent/WEB-INF/sql/constraint/constEx3.sql
@@ -26,3 +26,23 @@ VALUES ('b', 50);
 
 COMMIT;
 
+-- primary key, foreign key
+drop table const11;
+CREATE TABLE const11
+(
+id NUMBER,
+name VARCHAR2(30),
+dno NUMBER(2) NOT NULL,
+PRIMARY KEY (id),
+FOREIGN KEY (dno) REFERENCES const08_dep
+);
+
+
+-- 제약사항을 보는 쿼리
+
+SELECT c1.constraint_name, constraint_type,
+c2.table_name, c2.column_name
+FROM user_constraints c1 JOIN user_cons_columns c2 ON
+c1.constraint_name = c2.constraint_name;
+
+
