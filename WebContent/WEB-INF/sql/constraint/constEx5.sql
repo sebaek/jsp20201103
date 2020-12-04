9db9aecad6544e751fc63a7bc1f427df60ebcd6c
sebaek <sebaek@gmail.com>
2020-12-04 10:14:13 +0900

    add constraints

---
+++ WebContent/WEB-INF/sql/constraint/constEx5.sql
@@ -0,0 +1,34 @@
+-- 제약 사항 추가
+DROP TABLE emp_copy;
+
+CREATE TABLE emp_copy
+AS
+SELECT * FROM employee;
+
+SELECT * FROM emp_copy;
+
+SELECT * FROM user_constraints
+WHERE table_name='EMPLOYEE';
+
+SELECT * FROM user_constraints
+WHERE table_name='EMP_COPY';
+
+ALTER TABLE emp_copy
+ADD PRIMARY KEY (eno);
+
+DROP TABLE dept_copy;
+
+CREATE TABLE dept_copy
+AS
+SELECT * FROM department;
+
+ALTER TABLE dept_copy
+ADD CONSTRAINT dept_copy_dno_pk PRIMARY KEY (dno);
+
+SELECT * FROM user_constraints
+WHERE table_name='DEPT_COPY';
+
+-- 책 (259 쪽)
+
+
+
