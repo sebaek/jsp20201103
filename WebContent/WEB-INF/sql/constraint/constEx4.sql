66f428b4806a64c1ca66dde59bcc5cdc7a3a6066
sebaek <sebaek@gmail.com>
2020-12-04 09:32:01 +0900

    constraint name

--- WebContent/WEB-INF/sql/constraint/constEx4.sql
+++ WebContent/WEB-INF/sql/constraint/constEx4.sql
@@ -18,6 +18,23 @@ SELECT * FROM user_constraints
 WHERE constraint_name LIKE 'CUSTOMER%';
 
 
+-- 책 (253쪽)
+DROP TABLE customer;
+CREATE TABLE customer (
+  id VARCHAR2(20) ,
+  pwd VARCHAR2(20) CONSTRAINT customer_pwd NOT NULL,
+  name VARCHAR2(20) CONSTRAINT customer_name_nn NOT NULL,
+  phone VARCHAR2(30),
+  address VARCHAR2(100),
+  CONSTRAINT customer_id_pk PRIMARY KEY (id)
+);
+SELECT * FROM user_constraints
+WHERE constraint_name LIKE 'CUSTOMER%';
+
+-- 책 (255쪽)
+-- 책 (256쪽)
+
+
 
 
 
