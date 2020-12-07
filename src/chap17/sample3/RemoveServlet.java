28d3955c91343f2cdc5580f25215727595c8ae62
sebaek <sebaek@gmail.com>
2020-12-07 10:25:41 +0900

    delete

---
+++ src/chap17/sample3/RemoveServlet.java
@@ -0,0 +1,47 @@
+package chap17.sample3;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class RemoveServlet
+ */
+@WebServlet(name = "RemoveServlet2",
+urlPatterns = { "/sample3/post/remove" })
+public class RemoveServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public RemoveServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String id = request.getParameter("id");
+		
+		String sql = "DELETE FROM post WHERE id=?";
+		
+		// delete jdbc code
+		
+		response.sendRedirect(request.getContextPath() + "/sample3/post/main");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
