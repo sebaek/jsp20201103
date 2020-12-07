c8a37102c449920d868a0fb5d3c000e8ceece752
sebaek <sebaek@gmail.com>
2020-12-07 09:36:19 +0900

    deatil

--- src/chap17/sample3/DetailServlet.java
+++ src/chap17/sample3/DetailServlet.java
@@ -1,6 +1,11 @@
 package chap17.sample3;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -36,6 +41,61 @@ public class DetailServlet extends HttpServlet {
 		String path = "/WEB-INF/view/chap17/detail.jsp";
 		request.setAttribute("post", post);
 		request.getRequestDispatcher(path).forward(request, response);
+	}
+
+	private Post getPost(String id) {
+		Post post = null;
+		Connection con = null;
+		PreparedStatement stmt = null;
+		ResultSet rs = null;
+		
+		String url = "jdbc:oracle:thin:@localhost:1521:orcl";
+		String user = "c##mydbms";
+		String password = "admin";
+		String sql = "SELECT id, title, body FROM post WHERE id=?";
+
+		try {
+			// 1. class loading
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+
+			// 2. connection
+			con = DriverManager.getConnection(url, user, password);
+			// 3. statement
+			stmt = con.prepareStatement(sql);
+			stmt.setInt(1, Integer.parseInt(id));
+			
+			// 4. query
+			rs = stmt.executeQuery();
+			// 5. resultset
+			if (rs.next()) {
+				post = new Post();
+				post.setId(rs.getInt(1));
+				post.setTitle(rs.getString(2));
+				post.setBody(rs.getString(3));
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			// 6. close
+			if (stmt != null) {
+				try {
+					stmt.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+			if (con != null) {
+				try {
+					con.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		
+		
+		return post;
 	}
 
 	/**
